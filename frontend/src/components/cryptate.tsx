994dfa43fd1d079813b4c4dd875016c3aed71b8a
Carl <meatballsofmeatballs@gmail.com>
2023-09-07 22:57:48 +0200

    more work

--- frontend/src/components/cryptate.tsx
+++ frontend/src/components/cryptate.tsx
@@ -8,6 +8,9 @@ import {
   Image,
   SimpleGrid,
   NumberInput,
+  Stack,
+  Button,
+  Divider,
 } from '@mantine/core';
 import { Dropzone, FileWithPath, IMAGE_MIME_TYPE } from '@mantine/dropzone';
 import { Group, Text, rem } from '@mantine/core';
@@ -16,18 +19,30 @@ import { useState } from 'react';
 
 export default function Cryptate() {
   const [value, setValue] = useState('Rail Fence Cipher');
+  const [file, setFile] = useState<FileWithPath[]>([]);
 
   const options = () => {
     if (value === 'Rail Fence Cipher') {
       return (
-        <Group>
-          <Checkbox>Encrypt</Checkbox>
-          <Checkbox>Decrypt</Checkbox>
-        </Group>
+        <Stack>
+          <NumberInput
+            label="Rail Count"
+            withAsterisk
+            defaultValue={1}
+            decimalSeparator=","
+            hideControls
+          />
+          <TextInput
+            label="Message"
+            withAsterisk
+            placeholder="Input Message"
+            w={615}
+          ></TextInput>
+        </Stack>
       );
     } else if (value === 'Random') {
       return (
-        <Group>
+        <Stack>
           <NumberInput
             label="Key"
             withAsterisk
@@ -36,26 +51,25 @@ export default function Cryptate() {
             hideControls
           />
           <TextInput
-                label="Message"
-                withAsterisk
-                placeholder="Input Message"
-                w={615}>
-            
-          </TextInput>
-        </Group>
+            label="Message"
+            withAsterisk
+            placeholder="Input Message"
+            w={615}
+          ></TextInput>
+        </Stack>
       );
     } else if (value === 'Cesar Cipher') {
       return (
-        <Group>
+        <Stack>
           <Checkbox>Encrypt</Checkbox>
           <Checkbox>Decrypt</Checkbox>
-        </Group>
+        </Stack>
       );
     }
   };
 
   return (
-    <Fieldset legend="Cryptate" h={500}>
+    <Fieldset legend="Cryptate">
       <SegmentedControl
         onChange={(value) => {
           setValue(value);
@@ -63,9 +77,80 @@ export default function Cryptate() {
         fullWidth
         data={['Rail Fence Cipher', 'Random', 'Cesar Cipher']}
       ></SegmentedControl>
-      <Fieldset legend="Settings" m={20}>
-      {options()}
-      </Fieldset>
+      <br />
+      <Dropzone
+        onDrop={setFile}
+        onReject={(files) => console.log('rejected files', files)}
+        maxSize={3 * 1024 ** 2}
+        m={20}
+        accept={IMAGE_MIME_TYPE}
+      >
+        <Group
+          justify="center"
+          gap="xl"
+          mih={220}
+          style={{ pointerEvents: 'none' }}
+        >
+          <Dropzone.Accept>
+            <IconUpload
+              style={{
+                width: rem(52),
+                height: rem(52),
+                color: 'var(--mantine-color-blue-6)',
+              }}
+              stroke={1.5}
+            />
+          </Dropzone.Accept>
+          <Dropzone.Reject>
+            <IconX
+              style={{
+                width: rem(52),
+                height: rem(52),
+                color: 'var(--mantine-color-red-6)',
+              }}
+              stroke={1.5}
+            />
+          </Dropzone.Reject>
+          <Dropzone.Idle>
+            <IconPhoto
+              style={{
+                width: rem(52),
+                height: rem(52),
+                color: 'var(--mantine-color-dimmed)',
+              }}
+              stroke={1.5}
+            />
+          </Dropzone.Idle>
+
+          <div>
+            <Text size="xl" inline>
+              Drag images here or click to select files
+            </Text>
+            <Text size="sm" c="dimmed" inline mt={7}>
+              Attach as many files as you like, each file should not exceed 5mb
+            </Text>
+          </div>
+        </Group>
+      </Dropzone>
+      <Group grow>
+        <Fieldset legend="Image" m={20} radius="md">
+          <Image
+            src={file[0] ? URL.createObjectURL(file[0]) : null}
+            alt="Uploaded Image"
+            h={300}
+            fit="contain"
+            fallbackSrc="https://placehold.co/600x400?text=Placeholder"
+          />
+        </Fieldset>
+        <Stack>
+          <Fieldset legend="Settings" m={20} radius="md">
+            {options()}
+          </Fieldset>
+          <Button size="lg" color="teal" mx={20}>
+            Encrypt
+          </Button>
+        </Stack>
+      </Group>
     </Fieldset>
   );
 }
