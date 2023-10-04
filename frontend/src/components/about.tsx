fba5a57e47e6c976a0e6fc583ffc8983d806da3a
Carl <meatballsofmeatballs@gmail.com>
2023-10-04 22:02:30 +0200

    add about page

---
+++ frontend/src/components/about.tsx
@@ -0,0 +1,9 @@
+import { Container } from '@mantine/core';
+
+export default function About() {
+    return (
+        <Container fluid>
+            <h1> About </h1>
+        </Container>
+    )
+}
